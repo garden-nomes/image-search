096b612740056177057441182e1640e1b1c54979
noaner <noahbweiner@gmail.com>
2017-05-19 10:50:50 -0700

    use default mongo uri env key name

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const config = {
   port:      process.env.PORT      || 8000,
-  mongoUri:  process.env.MONGO_URI || 'mongodb://localhost:27017/image-search',
+  mongoUri:  process.env.MONGODB_URI || 'mongodb://localhost:27017/image-search',
   cseId:     process.env.CSE_ID,
   cseApiKey: process.env.CSE_API_KEY
 };
